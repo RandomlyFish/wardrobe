15718c6f9f00099d195a337312c7894747936064
tazercopter <yaseenelgamal20094@gmail.com>
2026-01-18 18:39:49 +0200

    make ui support category, group, and cosmetic assets

--- src/main/java/dev/hardaway/wardrobe/ui/WardrobePage.java
+++ src/main/java/dev/hardaway/wardrobe/ui/WardrobePage.java
@@ -12,31 +12,33 @@ import com.hypixel.hytale.protocol.ServerCameraSettings;
 import com.hypixel.hytale.protocol.packets.camera.SetServerCamera;
 import com.hypixel.hytale.protocol.packets.interface_.CustomPageLifetime;
 import com.hypixel.hytale.protocol.packets.interface_.CustomUIEventBindingType;
-import com.hypixel.hytale.server.core.cosmetics.CosmeticType;
 import com.hypixel.hytale.server.core.entity.entities.player.pages.InteractiveCustomUIPage;
+import com.hypixel.hytale.server.core.modules.entity.component.TransformComponent;
 import com.hypixel.hytale.server.core.ui.builder.EventData;
 import com.hypixel.hytale.server.core.ui.builder.UICommandBuilder;
 import com.hypixel.hytale.server.core.ui.builder.UIEventBuilder;
 import com.hypixel.hytale.server.core.universe.PlayerRef;
 import com.hypixel.hytale.server.core.universe.world.storage.EntityStore;
 import dev.hardaway.wardrobe.cosmetic.asset.CosmeticAsset;
+import dev.hardaway.wardrobe.cosmetic.asset.category.CosmeticCategory;
+import dev.hardaway.wardrobe.cosmetic.asset.category.CosmeticGroup;
 import it.unimi.dsi.fastutil.objects.Object2IntMap;
 import it.unimi.dsi.fastutil.objects.Object2IntOpenHashMap;
 
 import javax.annotation.Nonnull;
-import javax.annotation.Nullable;
-import java.util.ArrayList;
-import java.util.Comparator;
-import java.util.List;
-import java.util.Locale;
-import java.util.stream.Collectors;
+import java.util.*;
 
 public class WardrobePage extends InteractiveCustomUIPage<WardrobePage.PageEventData> {
 
-    private CosmeticSubTab subTab;
-    private List<CosmeticAsset> cosmetics;
-    private CosmeticAsset cosmetic;
-    private ServerCameraSettings cameraSettings;
+    private final List<CosmeticCategory> categories = CosmeticCategory.getAssetMap().getAssetMap().values().stream().sorted(Comparator.comparing(CosmeticCategory::getOrder)).toList();
+    private final Map<CosmeticCategory, List<CosmeticGroup>> groupMap = HashMap.newHashMap(CosmeticCategory.getAssetMap().getAssetCount());
+    private final Map<CosmeticGroup, List<CosmeticAsset>> cosmeticMap = HashMap.newHashMap(CosmeticAsset.getAssetMap().getAssetCount());
+
+    private CosmeticCategory selectedCategory;
+    private CosmeticGroup selectedGroup;
+    private String searchQuery = "";
+
+    private ServerCameraSettings cameraSettings = new ServerCameraSettings();
 
     public WardrobePage(@Nonnull PlayerRef playerRef, @Nonnull CustomPageLifetime lifetime) {
         super(playerRef, lifetime, PageEventData.CODEC);
@@ -44,22 +46,33 @@ public class WardrobePage extends InteractiveCustomUIPage<WardrobePage.PageEvent
 
     @Override
     public void build(@Nonnull Ref<EntityStore> ref, @Nonnull UICommandBuilder commandBuilder, @Nonnull UIEventBuilder eventBuilder, @Nonnull Store<EntityStore> store) {
-        commandBuilder.append("Pages/Wardrobe/Wardrobe.ui");
+        commandBuilder.append("Wardrobe/Pages/Wardrobe.ui");
 
-        selectTab(commandBuilder, CosmeticTab.Head);
-        buildCosmeticList(ref, store, commandBuilder, eventBuilder, null);
+        for (int i = 0; i < categories.size(); i++) {
+            CosmeticCategory category = categories.get(i);
+            groupMap.put(category, new ArrayList<>());
+            commandBuilder.append("#Categories", "Wardrobe/Pages/Tab.ui");
+            String selector = "#Categories[" + i + "] #Button";
+            commandBuilder.set(selector + " #Selected.Background", "(TexturePath: \"../Icons/Categories/General.png\")");
+            eventBuilder.addEventBinding(CustomUIEventBindingType.Activating, selector, EventData.of("Category", category.getId()), false);
+        }
+
+        List<CosmeticGroup> sortedGroups = CosmeticGroup.getAssetMap().getAssetMap().values().stream().sorted(Comparator.comparing(CosmeticGroup::getOrder)).toList();
+        for (CosmeticGroup group : sortedGroups) {
+            groupMap.getOrDefault(CosmeticCategory.getAssetMap().getAsset(group.getCategory()), new ArrayList<>()).add(group);
+            cosmeticMap.put(group, new ArrayList<>());
+        }
+
+        List<CosmeticAsset> sortedCosmetics = CosmeticAsset.getAssetMap().getAssetMap().values().stream().sorted(Comparator.comparing(CosmeticAsset::getId)).toList();
+        for (CosmeticAsset asset : sortedCosmetics) {
+            cosmeticMap.get(CosmeticGroup.getAssetMap().getAsset(asset.getGroup())).add(asset);
+        }
+
+        CosmeticCategory selectedCategory = categories.getFirst();
+        selectCategory(commandBuilder, eventBuilder, selectedCategory);
 
         eventBuilder.addEventBinding(CustomUIEventBindingType.ValueChanged, "#SearchField", EventData.of("@SearchQuery", "#SearchField.Value"), false);
 
-        for (CosmeticTab tab : CosmeticTab.values()) {
-            eventBuilder.addEventBinding(CustomUIEventBindingType.Activating, "#Tab" + tab.toString().replace("_", "") + " #Button", EventData.of("Tab", tab.toString()), false);
-        }
-
-        for (CosmeticSubTab tab : CosmeticSubTab.values()) {
-            eventBuilder.addEventBinding(CustomUIEventBindingType.Activating, "#Tab" + tab.toString().replace("_", "") + " #Button", EventData.of("SubTab", tab.toString()), false);
-        }
-
-        cameraSettings = new ServerCameraSettings();
         cameraSettings.isFirstPerson = false;
         cameraSettings.rotation = new Direction();
         playerRef.getPacketHandler().writeNoCache(new SetServerCamera(ClientCameraView.ThirdPerson, true, cameraSettings));
@@ -75,12 +88,20 @@ public class WardrobePage extends InteractiveCustomUIPage<WardrobePage.PageEvent
         UICommandBuilder commandBuilder = new UICommandBuilder();
         UIEventBuilder eventBuilder = new UIEventBuilder();
 
-        if (data.tab != null) selectTab(commandBuilder, CosmeticTab.valueOf(data.tab));
-        if (data.subTab != null) selectSubTab(commandBuilder, CosmeticSubTab.valueOf(data.subTab));
-        buildCosmeticList(ref, store, commandBuilder, eventBuilder, data.searchQuery);
+        if (data.searchQuery != null) buildCosmeticList(commandBuilder, eventBuilder, data.searchQuery);
+        if (data.category != null) selectCategory(commandBuilder, eventBuilder, CosmeticCategory.getAssetMap().getAsset(data.category));
+        if (data.group != null) selectGroup(commandBuilder, eventBuilder, CosmeticGroup.getAssetMap().getAsset(data.group));
+        if (data.cosmetic != null) wearCosmetic(commandBuilder, eventBuilder, CosmeticAsset.getAssetMap().getAsset(data.cosmetic));
 
-        // TODO: make the buttons rotate the body/head
-        sendUpdate(commandBuilder);
+        if (data.direction != null) {
+            TransformComponent transformComponent = store.getComponent(ref, TransformComponent.getComponentType());
+
+            if (data.direction.equals("Right")) {
+            } else {
+            }
+        }
+
+        sendUpdate(commandBuilder, eventBuilder, false);
     }
 
     @Override
@@ -90,111 +111,95 @@ public class WardrobePage extends InteractiveCustomUIPage<WardrobePage.PageEvent
         playerRef.getPacketHandler().writeNoCache(new SetServerCamera(ClientCameraView.FirstPerson, false, null));
     }
 
-    private void buildCosmeticList(Ref<EntityStore> ref, Store<EntityStore> store, UICommandBuilder commandBuilder, UIEventBuilder eventBuilder, @Nullable String searchQuery) {
-        commandBuilder.clear("#Parts");
-
-        CosmeticType cosmeticType = null;
-        for (CosmeticType type : CosmeticType.values()) {
-            if (type.toString().equalsIgnoreCase(subTab.toString()) || (type == CosmeticType.CAPES && subTab.toString().equals("Cape"))) {
-                cosmeticType = type;
-                break;
-            }
+    public void selectCategory(UICommandBuilder commandBuilder, UIEventBuilder eventBuilder, CosmeticCategory category) {
+        for (int i = 0; i < categories.size(); i++) {
+            commandBuilder.set("#Categories[" + i + "] #Button #Selected.Visible", false);
         }
-        if (cosmeticType == null) return;
+        commandBuilder.set("#Categories[" + categories.indexOf(category) + "] #Button #Selected.Visible", true);
 
-        ArrayList<CosmeticAsset> tabCosmetics = new ArrayList<>();
-        for (CosmeticAsset cosmetic : CosmeticAsset.getAssetMap().getAssetMap().values()) {
-//            if (cosmetic.getGroup() == cosmeticType) tabCosmetics.add(cosmetic);
+        commandBuilder.clear("#Groups");
+        List<CosmeticGroup> groups = groupMap.get(category);
+        for (int i = 0; i < groups.size(); i++) {
+            CosmeticGroup group = groups.get(i);
+            commandBuilder.append("#Groups", "Wardrobe/Pages/Tab.ui");
+            // TODO: icon
+            eventBuilder.addEventBinding(CustomUIEventBindingType.Activating, "#Groups[" + i + "] #Button", EventData.of("Group", group.getId()), false);
         }
 
-        if (tabCosmetics.isEmpty()) return;
-
-        if (searchQuery == null || searchQuery.isEmpty()) {
-            cosmetics = tabCosmetics.stream().sorted().sorted(Comparator.comparing(CosmeticAsset::getId)).collect(Collectors.toList());
+        if (groups.isEmpty()) {
+            commandBuilder.set("#CategoryLabelArrow.Visible", false);
+            commandBuilder.set("#CategoryName.Text", "");
         } else {
-            Object2IntMap<CosmeticAsset> map = new Object2IntOpenHashMap<>(tabCosmetics.size());
+            commandBuilder.set("#CategoryLabelArrow.Visible", true);
+            commandBuilder.set("#CategoryName.Text", category.getId());
+        }
 
-            for (CosmeticAsset value : tabCosmetics) {
+        selectedCategory = category;
+
+        CosmeticGroup selectedGroup = groups.getFirst();
+        selectGroup(commandBuilder, eventBuilder, selectedGroup);
+    }
+
+    public void selectGroup(UICommandBuilder commandBuilder, UIEventBuilder eventBuilder, CosmeticGroup group) {
+        List<CosmeticGroup> groups = groupMap.get(selectedCategory);
+        for (int i = 0; i < groups.size(); i++) {
+            commandBuilder.set("#Groups[" + i + "] #Button #Selected.Visible", false);
+        }
+        commandBuilder.set("#Groups[" + groups.indexOf(group) + "] #Button #Selected.Visible", true);
+        commandBuilder.set("#SubCategoryName.Text", group.getId());
+        selectedGroup = group;
+
+        buildCosmeticList(commandBuilder, eventBuilder, searchQuery);
+    }
+
+    public void buildCosmeticList(UICommandBuilder commandBuilder, UIEventBuilder eventBuilder, String searchQuery) {
+        commandBuilder.clear("#Cosmetics");
+
+        List<CosmeticAsset> cosmetics = cosmeticMap.get(selectedGroup);
+        if (!searchQuery.isEmpty()) {
+            Object2IntMap<CosmeticAsset> map = new Object2IntOpenHashMap<>(cosmetics.size());
+
+            for (CosmeticAsset value : cosmetics) {
                 int fuzzyDistance = StringCompareUtil.getFuzzyDistance(value.getId(), searchQuery, Locale.ENGLISH);
                 if (fuzzyDistance > 0) {
                     map.put(value, fuzzyDistance);
                 }
             }
 
-            cosmetics = map.keySet().stream().sorted().sorted(Comparator.comparingInt(map::getInt).reversed()).limit(20L).collect(Collectors.toList());
+            cosmetics = map.keySet().stream().sorted().sorted(Comparator.comparingInt(map::getInt).reversed()).limit(20L).toList();
         }
 
-        int bound = cosmetics.size();
-        for(int i = 0; i < bound; ++i) {
+        for (int i = 0; i < cosmetics.size(); i++) {
             CosmeticAsset cosmetic = cosmetics.get(i);
-            String selector = "#Parts[" + i + "]";
-            commandBuilder.append("#Parts", "Pages/Wardrobe/Cosmetic.ui");
+            commandBuilder.append("#Cosmetics", "Wardrobe/Pages/Cosmetic.ui");
+            String selector = "#Cosmetics[" + i + "]";
             commandBuilder.set(selector + " #Button.Text", cosmetic.getId());
-            // FIXME
-//            if (cosmetic.getIcon() != null) commandBuilder.setObject(selector + " #Icon.Background", new PatchStyle(Value.of(cosmetic.getId())));
+            // TODO: icon
             eventBuilder.addEventBinding(CustomUIEventBindingType.Activating, selector + " #Button", EventData.of("Cosmetic", cosmetic.getId()), false);
         }
 
-//        if (!cosmetics.isEmpty()) {
-//            if (!cosmetics.contains(selectedCosmetic)) {
-//                selectModel(ref, store, cosmetics.getFirst(), commandBuilder);
-//            } else if (selectedCosmetic != null) {
-//                selectModel(ref, store, selectedCosmetic, commandBuilder);
-//            }
-//        }
+        // TODO: highlight cosmetics worn
+
+        this.searchQuery = searchQuery;
     }
 
-    public void selectTab(UICommandBuilder commandBuilder, CosmeticTab selected) {
-        for (CosmeticTab tab : CosmeticTab.values()) {
-            commandBuilder.set("#Tab" + tab.toString().replace("_", "") + " #Button #Selected.Visible", false);
-        }
-
-        for (CosmeticSubTab tab : CosmeticSubTab.values()) {
-            commandBuilder.set("#Tab" + tab.toString().replace("_", "") + ".Visible", false);
-        }
-
-        if (selected.subTabs.isEmpty()) {
-            commandBuilder.set("#CategoryLabelArrow.Visible", false);
-            commandBuilder.set("#CategoryName.Text", "");
-        } else {
-            commandBuilder.set("#CategoryLabelArrow.Visible", true);
-            commandBuilder.set("#CategoryName.Text", selected.toString().replace("_", " "));
-        }
-
-        commandBuilder.set("#Tab" + selected.toString().replace("_", "") + " #Button #Selected.Visible", true);
-
-        for (CosmeticSubTab subTab : CosmeticSubTab.values()) {
-            if (selected.subTabs.contains(subTab))
-                commandBuilder.set("#Tab" + subTab.toString().replace("_", "") + ".Visible", true);
-        }
-
-        commandBuilder.set("#Tab" + selected.defaultSubTab.toString().replace("_", "") + ".Visible", true);
-        selectSubTab(commandBuilder, selected.defaultSubTab);
-    }
-
-    public void selectSubTab(UICommandBuilder commandBuilder, CosmeticSubTab selected) {
-        for (CosmeticSubTab tab : CosmeticSubTab.values()) {
-            commandBuilder.set("#Tab" + tab.toString().replace("_", "") + " #Button #Selected.Visible", false);
-        }
-
-        commandBuilder.set("#Tab" + selected.toString().replace("_", "") + " #Button #Selected.Visible", true);
-        commandBuilder.set("#SubCategoryName.Text", selected.toString().replace("_", " "));
-
-        this.subTab = selected;
+    public void wearCosmetic(UICommandBuilder commandBuilder, UIEventBuilder eventBuilder, CosmeticAsset cosmetic) {
+        // TODO: apply cosmetic and reset cosmetic list
+        buildCosmeticList(commandBuilder, eventBuilder, searchQuery);
     }
 
     public static class PageEventData {
         public static final BuilderCodec<PageEventData> CODEC = BuilderCodec.builder(PageEventData.class, PageEventData::new)
                 .append(new KeyedCodec<>("@SearchQuery", Codec.STRING), (entry, s) -> entry.searchQuery = s, (entry) -> entry.searchQuery).add()
-                .append(new KeyedCodec<>("Tab", Codec.STRING), (entry, s) -> entry.tab = s, (entry) -> entry.tab).add()
-                .append(new KeyedCodec<>("SubTab", Codec.STRING), (entry, s) -> entry.subTab = s, (entry) -> entry.subTab).add()
+                .append(new KeyedCodec<>("Category", Codec.STRING), (entry, s) -> entry.category = s, (entry) -> entry.category).add()
+                .append(new KeyedCodec<>("Group", Codec.STRING), (entry, s) -> entry.group = s, (entry) -> entry.group).add()
                 .append(new KeyedCodec<>("Cosmetic", Codec.STRING), (entry, s) -> entry.cosmetic = s, (entry) -> entry.cosmetic).add()
                 .append(new KeyedCodec<>("Direction", Codec.STRING), (entry, s) -> entry.direction = s, (entry) -> entry.direction).add()
                 .build();
 
         private String searchQuery;
-        private String tab;
-        private String subTab;
+        private String category;
+        private String group;
         private String cosmetic;
         private String direction;
     }
