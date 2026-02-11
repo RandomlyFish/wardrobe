5fda1096c75c02252cf436b1d5c4025ce0629066
tazercopter <yaseenelgamal20094@gmail.com>
2026-02-11 04:03:50 +0200

    fix valid icon paths

--- src/main/java/dev/hardaway/wardrobe/api/property/validator/WardrobeValidators.java
+++ src/main/java/dev/hardaway/wardrobe/api/property/validator/WardrobeValidators.java
@@ -6,7 +6,7 @@ import com.hypixel.hytale.server.core.asset.common.CommonAssetValidator;
 import dev.hardaway.wardrobe.api.property.WardrobeProperties;
 
 public class WardrobeValidators {
-    public static final CommonAssetValidator ICON = new CommonAssetValidator("png", "Icons/Cosmetics", "Icons/Wardrobe", "Icons/Models", "Icons/ItemGenerated", "Icons/ModelsGenerated");
+    public static final CommonAssetValidator ICON = new CommonAssetValidator("png", "Icons/Cosmetics", "Icons/Wardrobe", "Icons/Models", "Icons/ItemsGenerated", "Icons/ModelsGenerated");
     public static final ArrayValidator<String> COLOR = new ArrayValidator<>(HexcodeValidator.INSTANCE);
 
     @Deprecated(forRemoval = true)
